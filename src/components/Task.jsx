713d1c8772f1dd418a244eb7d8ed4ffe24a2e3af
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-02 05:28:10 +0530

    Component Added For Showing Single Task

---
+++ src/components/Task.jsx
@@ -0,0 +1,28 @@
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import "../index.css";
+
+const Task = ({ task, onDelete, onEdit }) => {
+  return (
+    <div>
+      <div className="task">
+        <div>
+          <p className="taskName">
+            <span className="textBold">Task Name:</span> {task.text}
+          </p>
+          <p className="taskDate">
+            <span className="textBold">Date of Completion:</span> {task.day}
+          </p>
+        </div>
+        <div>
+          <p>
+            <FaTimes onClick={() => onDelete(task.id)} className="delIcon" />
+          </p>
+          <p>
+            <FaPencilAlt onClick={() => onEdit(task.id)} className="editIcon" />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Task;
