0778a44fbdcf34ea0fbf1a24f37db1b2c946b4b8
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-02 05:29:09 +0530

    mapped over all task and shown all the task

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,13 @@
+import Task from "./Task";
+import "../index.css";
+
+const Tasks = ({ tasks, onDelete, onEdit }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onEdit={onEdit} />
+      ))}
+    </>
+  );
+};
+export default Tasks;
