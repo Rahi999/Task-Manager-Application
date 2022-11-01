856b1dbf0ced74c460e532d61592fab23c04fc4c
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-02 05:27:04 +0530

    Header Add/Close Button Added

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+import "../index.css";
+
+const Header = ({ showForm, changeTextAndColor }) => {
+  return (
+    <header className="header">
+      <h2 className="app-header">Task Manager App</h2>
+      <Button
+        onClick={showForm}
+        color={changeTextAndColor ? "red" : "green"}
+        text={changeTextAndColor ? "Close" : "Add"}
+      />
+    </header>
+  );
+};
+export default Header;
